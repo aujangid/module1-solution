76076cd6bf57c07ebd67c2965e3a2a1724f30d26
Ashok <ashokjangid.7@gmail.com>
2020-02-12 23:38:05 +0530

    assignment1-basic

--- app.js
+++ app.js
@@ -2,11 +2,28 @@
 
 angular.module("LunchCheck",[])
        .controller("LunchCheckController",LunchCheckFun);
-LunchCheckFun.$inject=["$scope
+LunchCheckFun.$inject=["$scope"];
 function LunchCheckFun($scope){
+$scope.dishes="";
+$scope.error_msg="";
 
+$scope.check_dishes=function(){
+var stringInputDishes = $scope.dishes;
+var stringInputSplitArray = stringInputDishes.split(",");
+var stringInput = stringInputSplitArray.length;
+if(stringInputDishes===""){
+  $scope.error_msg="Please enter data first";
+}
+else if (stringInput<4) {
+  $scope.error_msg="Enjoy!";
+}
+else if (stringInput>3) {
+  $scope.error_msg="Too much!"
+}
+          console.log(stringInput);
+           };
 };
 
 
-,[]}
+}
 )();
